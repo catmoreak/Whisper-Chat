3b609206f085b781074f6ec69b477f2c3793e7e1
catmoreak <72437057+catmoreak@users.noreply.github.com>
2026-01-25 13:47:09 +0530

    routes added in the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
+import React, { useState } from 'react';
+import RoomSetup from './Roomsetup';
+import { generateRoomCode } from './lib/utils';
+import { Toaster } from 'react-hot-toast';
 
-import './App.css'
+const App: React.FC = () => {
+  const [roomId, setRoomId] = useState<string | null>(null);
+  const [inputRoom, setInputRoom] = useState('');
 
-function App() {
 
+  const createRoom = () => {
+    const newRoom = generateRoomCode();
+    setRoomId(newRoom);
+  };
+
+  const joinRoom = () => {
+    if (inputRoom.length === 5 && /^\d{5}$/.test(inputRoom)) {
+      setRoomId(inputRoom);
+    } else {
+      alert('Please enter a valid 5-digit room code');
+    }
+  };
+
+  if (roomId) {
+    return (
+      <>
+        <Toaster />
+     
+      </>
+    );
+  }
 
   return (
     <>
-      <div className="App">
-        <p>Whisper Chat</p>
-      </div>
-    
+      <Toaster />
+      <RoomSetup
+        inputRoom={inputRoom}
+        setInputRoom={setInputRoom}
+        createRoom={createRoom}
+        joinRoom={joinRoom}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
