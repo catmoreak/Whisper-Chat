909b67b1fecdab16f17caeff917bce9d3e8fb1b0
catmoreak <72437057+catmoreak@users.noreply.github.com>
2026-02-08 21:27:51 +0530

    routes for media upload through uploadthing

---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,16 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+      return { userId: "user" };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
