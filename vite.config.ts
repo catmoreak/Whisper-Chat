38b73eec73f9daad87fd237341c8f3000b593aa4
catmoreak <72437057+catmoreak@users.noreply.github.com>
2026-02-11 20:52:00 +0530

    updated .gitignore and package.json for Vercel configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.UPLOADTHING_TOKEN': JSON.stringify(process.env.UPLOADTHING_TOKEN),
+  },
 })
