9c951e9470a43cf28c53419e1f73cfc2b6bbc28d
catmoreak <72437057+catmoreak@users.noreply.github.com>
2026-02-13 20:36:24 +0530

    vercel configuration doesnt work so del uplaodthing

--- src/lib/uploadthing.ts
+++
@@ -1,7 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "../../api/uploadthing/[...slug]";
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>({
-    url: "/api/uploadthing",
-  });
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.UPLOADTHING_TOKEN': JSON.stringify(process.env.UPLOADTHING_TOKEN),
-  },
 })
